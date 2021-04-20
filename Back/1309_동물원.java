9f390e037e72536c1cb97f85f31e0065916d60c7
YoonMinSeok <81290520+YoonMinSeok@users.noreply.github.com>
2021-04-20 21:49:23 +0900

    Create 1309_동물원.java

---
+++ Back/1309_동물원.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static int N;
+    static int dp[][];
+   
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        
+        N = sc.nextInt();
+        
+        dp = new int[N+1][3];
+        dp[1][0] = 1;
+        dp[1][1] = 1;
+        dp[1][2] = 1;
+        
+        for(int i=2; i <= N; i++){
+            dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2])%9901;
+            dp[i][1] = (dp[i-1][0] + dp[i-1][2])%9901;
+            dp[i][2] = (dp[i-1][0] + dp[i-1][1])%9901;
+        }
+       
+        System.out.println((dp[N][0]+dp[N][1]+dp[N][2])%9901);
+    }
+}
