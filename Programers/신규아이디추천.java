47924d50ad610e36f332f9b19683cbd28c848c74
YoonMinSeok <81290520+YoonMinSeok@users.noreply.github.com>
2021-05-12 16:40:45 +0900

    Create 신규아이디추천.java

---
+++ Programers/신규아이디추천.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String new_id) {
+        
+        String id = new_id.toLowerCase();
+        id = id.replaceAll("[^-_.a-z0-9]","");
+        id = id.replaceAll("[.]{2,}",".");
+        id = id.replaceAll("^[.]|[.]$","");
+        
+        if(id.equals("")){
+            id += "a";
+        }
+        
+        if(id.length() >= 16){
+            id = id.substring(0,15);
+            id = id.replaceAll("^[.]|[.]$","");
+        }
+        
+        if(id.length() <= 2){
+            while(id.length()< 3)
+                id += id.charAt(id.length()-1);
+        }
+        
+        return id;
+    }
+}
