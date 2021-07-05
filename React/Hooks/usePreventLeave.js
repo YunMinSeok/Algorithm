68a0afebbe793ba4176e54d5abd1323db60af3e1
YoonMinSeok <81290520+YoonMinSeok@users.noreply.github.com>
2021-07-05 17:35:29 +0900

    Create usePreventLeave.js

---
+++ React/Hooks/usePreventLeave.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+
+const usePreventLeave = () => {
+  const listener = (event) => {
+    event.preventDefault();
+    event.retrunValue = "";
+  };
+  const enablePrevent = () => window.addEventListener("beforeunload", listener);
+  const disablePrevent = () =>
+    window.removeEventListener("beforeunload", listener);
+  return { enablePrevent, disablePrevent };
+};
+
+const App = () => {
+  const { enablePrevent, disablePrevent } = usePreventLeave();
+  return (
+    <div className="App">
+      <button onClick={enablePrevent}>Protect</button>
+      <button onClick={disablePrevent}>unProtect</button>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
