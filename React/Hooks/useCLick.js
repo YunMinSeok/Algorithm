64d48e295e45345e65ed4a15be3c21b6e4ac307a
YoonMinSeok <81290520+YoonMinSeok@users.noreply.github.com>
2021-07-05 16:46:52 +0900

    Create useCLick.js

---
+++ React/Hooks/useCLick.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+
+const useClick = (onClick) =>{
+  if(typeof onClick !== "function"){
+    return;
+  }
+  const element = useRef();
+  useEffect(()=>{
+    if(element.current){
+      element.current.addEventListener("click",onClick);
+    }
+  
+    return () =>{
+      if(element.current){
+        element.current.removeEventListener("click",onCLick);
+      }
+    };
+  },[]);
+  return element;
+}
+
+const App = () => {
+  const sayHello = () => console.log("say Hello");
+  const title = useClick(sayHello);
+  return (
+    <div className="App">
+      <h1 ref ={title}>Hi</h1>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
