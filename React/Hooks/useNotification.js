23b3562374ef025889ccab0033ade5b00ae0f3cb
YoonMinSeok <81290520+YoonMinSeok@users.noreply.github.com>
2021-07-14 21:07:37 +0900

    Create useNotification.js

---
+++ React/Hooks/useNotification.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+
+const useNotification = (title, options) => {
+  if (!("Notification" in window)) {
+    return;
+  }
+  const fireNotif = () => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      new Notification(title, options);
+    }
+  };
+  return fireNotif;
+};
+
+const App = () => {
+  const triggerNotif = useNotification("Can I steal your kmichi?", {
+    body: "I love kimchi dont you"
+  });
+  return (
+    <div className="App" style={{ height: "1000vh" }}>
+      <button onClick={triggerNotif}>Hello</button>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
