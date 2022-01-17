2943d0b6ffbec0d3f8821d3c6c0cf9e87b281452
윤민석 <81290520+YoonMinSeok@users.noreply.github.com>
2022-01-17 23:10:05 +0900

    Create 124나라의 숫자.js

---
+++ Programers/124나라의 숫자.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    const numbers = [4,1,2];
+    let answer = '';
+    
+    while(n){
+        answer = numbers[n%3] +answer;
+        n = (n%3)==0 ? n/3 -1 : Math.floor(n/3);
+    }
+    
+    return answer;
+}
