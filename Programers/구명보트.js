11afc73d6e76b4237dd2a00ac5bbd16ee6a91b71
yo_onms <81290520+YoonMinSeok@users.noreply.github.com>
2022-02-05 23:01:37 +0900

    Create 구명보트.js

---
+++ Programers/구명보트.js
@@ -0,0 +1,15 @@
+function solution(people, limit) {
+    var answer = 0;
+    
+    people.sort((a,b)=> b-a);
+    
+    for(let i=0 ,j=people.length-1; i<=j; i++){
+        if(people[i]+people[j]>limit){ answer++;}
+        else{
+            answer++;
+            j--;
+        }
+    }
+    
+    return answer;
+}
