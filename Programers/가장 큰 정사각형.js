285b58b155350855016a9061d0c10c88f6ee16fa
yo_onms <81290520+YoonMinSeok@users.noreply.github.com>
2022-02-08 22:17:21 +0900

    Create 가장 큰 정사각형.js

---
+++ Programers/가장 큰 정사각형.js
@@ -0,0 +1,23 @@
+function solution(board)
+{
+    let answer = 0;
+    let row = board.length;
+    let column = board[0].length;
+    
+    if(row <= 1 || column <= 1) return 1;
+    
+    for(let i=1; i < row; i++){
+        for(let j=1; j < column; j++){
+            if(board[i][j]>0){
+                let up = board[i-1][j];
+                let left = board[i][j-1];
+                let cross = board[i-1][j-1];
+                let min = Math.min(up,left,cross);
+                board[i][j] = min+1;
+                answer = Math.max(answer,board[i][j])
+            }
+        }
+    }
+
+    return answer*answer;
+}
