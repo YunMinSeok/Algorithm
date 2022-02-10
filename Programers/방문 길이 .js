d8df26ea9e75fd448ac63f941a5e8d16575e5af2
yo_onms <81290520+YoonMinSeok@users.noreply.github.com>
2022-02-10 23:30:50 +0900

    Create 방문 길이 .js

---
+++ Programers/방문 길이 .js
@@ -0,0 +1,21 @@
+function solution(dirs) {
+    let answer = 0;
+    const move = { U: [0,1], D : [0,-1], R : [1,0], L : [-1,0]};
+    
+    let check = new Set();
+    let now = [0,0];
+    
+    for(let i=0; i < dirs.length; i++){
+        let nx = now[0] + move[dirs[i]][0];
+        let ny = now[1] + move[dirs[i]][1];
+        
+        if(nx < -5 || nx > 5 || ny > 5 || ny < -5) continue;
+        
+        check.add(""+ now[0] + now[1]+ nx + ny)
+        check.add(""+ nx + ny + now[0] + now[1])
+        
+        now = [nx,ny];
+    }
+    
+    return check.size/2;
+}
