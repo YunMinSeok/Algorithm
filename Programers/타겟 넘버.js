7bbf98bff4dc8397df848e6c70145d329e52a1e8
yo_onms <81290520+YoonMinSeok@users.noreply.github.com>
2022-03-13 15:06:02 +0900

    Create 타겟 넘버.js

---
+++ Programers/타겟 넘버.js
@@ -0,0 +1,16 @@
+function solution(numbers, target) {
+    var answer = 0;
+   
+    dfs(0,0);
+    function dfs(level,sum){
+        if(level === numbers.length){
+            if(sum === target){
+                answer++;
+            }
+            return;
+        }
+        dfs(level+1, sum+numbers[level])
+        dfs(level+1, sum-numbers[level])
+    }
+    return answer;
+}
