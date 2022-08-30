7a68fc3c12600f4bb51e42d0419feb70746b2acd
yo_onms <81290520+YunMinSeok@users.noreply.github.com>
2022-08-30 14:03:39 +0900

    Update 최소직사각형.js

--- Programers/최소직사각형.js
+++ Programers/최소직사각형.js
@@ -1,21 +1,21 @@
 function solution(sizes) {
-    var answer = 0;
+    let answer = 0;
+    
+    for(let i in sizes){
+        sizes[i].sort((a,b)=> b-a);
+    }
     
     let garo = 0;
     let sero = 0;
-    for(let i = 0; i < sizes.length; i++){
-        sizes[i].sort((a,b) => b-a);
-    }
     
-    for(let i = 0; i < sizes.length; i++){
+    for(let i in sizes){
         if(sizes[i][0] > garo){
-            garo = sizes[i][0];
+            garo = sizes[i][0]
         }
         if(sizes[i][1] > sero){
-            sero = sizes[i][1];
+            sero = sizes[i][1]
         }
     }
-    
-    answer = garo * sero;
+    answer = garo * sero
     return answer;
 }
