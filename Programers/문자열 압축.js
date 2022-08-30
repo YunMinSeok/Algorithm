7efc5ef783861ae6180b8fe49c97f7682dcdcbcb
yo_onms <81290520+YunMinSeok@users.noreply.github.com>
2022-08-30 14:49:17 +0900

    Update 문자열 압축.js

--- Programers/문자열 압축.js
+++ Programers/문자열 압축.js
@@ -1,22 +1,23 @@
 function solution(s) {
-    let min_len = s.length;
+    let answer = s.length;
     
-    for(let i=1; i<= s.length/2; i++){
+    for(let i=1; i <= s.length/2; i++){
         let prev = s.substr(0,i);
         let cnt = 1;
         let tmp = "";
-        for(let k =i; k<s.length; k+=i){
+        for(let k=i; k< s.length; k+=i){
             const cur = s.substr(k,i);
-            if(prev === cur) cnt++;
-            else{
-                tmp += (cnt > 1 ? cnt : "") +prev;
+            if(prev === cur){
+                cnt++
+            }else{
+                tmp += (cnt > 1 ? cnt : "") + prev;
                 cnt = 1;
                 prev = cur;
             }
         }
-        tmp += (cnt > 1 ? cnt : "") +prev;
-        min_len = Math.min(min_len, tmp.length)
+        tmp += (cnt > 1 ? cnt: "") + prev;
+        answer = Math.min(answer, tmp.length)
     }
     
-    return min_len;
+    return answer;
 }
