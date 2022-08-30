534330997cbb030c7d6a8a1cd56c5c4cc40aedea
yo_onms <81290520+YunMinSeok@users.noreply.github.com>
2022-08-30 13:34:19 +0900

    Update 신고 결과 받기.js

--- Programers/신고 결과 받기.js
+++ Programers/신고 결과 받기.js
@@ -1,32 +1,33 @@
-const transformObj =(report)=>{
-    const obj ={};
+function transformObj(report){
+    const obj = {};
     const set = [...new Set(report)];
-    set.forEach((log) => {
-        const tmp = log.split(' ');
-        if (obj[tmp[1]] === undefined) {
-          obj[tmp[1]] = [tmp[0]];
-        } else {
-          obj[tmp[1]] = [...obj[tmp[1]], tmp[0]];
-        }
-      });
-  return obj;
     
+    set.forEach((data)=>{
+        const tmp = data.split(" ");
+        
+        if(obj[tmp[1]] === undefined){
+            obj[tmp[1]] = [tmp[0]];
+        }else{
+            obj[tmp[1]] = [...obj[tmp[1]],tmp[0]]
+        }
+    })
+    
+    return obj
 }
 
-const solution=(id_list, report, k)=>{
+function solution(id_list, report, k) {
     const report_obj = transformObj(report);
+    const answer = Array.from({length: id_list.length}, ()=> 0)
     
-    const answer = Array.from({ length: id_list.length }, () => 0);
-
-     for (let i in report_obj) {
-        if (report_obj[i].length >= k) {
-          const ids = report_obj[i];
-          ids.forEach((id) => {
-            const index = id_list.indexOf(id);
-            answer[index]++;
-          });
+    for(let i in report_obj){
+        if(report_obj[i].length >= k){
+            const ids = report_obj[i];
+            ids.forEach((id)=>{
+                const index = id_list.indexOf(id);
+                answer[index]++;
+            })
         }
-      }
+    }
     
     return answer;
 }
